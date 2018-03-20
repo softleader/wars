5300b53ea227fb1833692579c0b2c237df1385a2
Matt S.Y. Ho <methodho@gmail.com>
2018-03-20 11:28:52 +0800

    switch to traditional deployment

--- src/main/java/tw/com/softleader/wars/WarsApplication.java
+++ src/main/java/tw/com/softleader/wars/WarsApplication.java
@@ -2,11 +2,18 @@ package tw.com.softleader.wars;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WarsApplication {
+public class WarsApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WarsApplication.class, args);
-	}
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(WarsApplication.class);
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(WarsApplication.class, args);
+  }
 }
